c9312c1d8c4e9c72c04fad00519fcd18d157bafb
RoyLiu <chance6687204@gmail.com>
2024-06-21 11:59:47 +0800

    feat: PolicySection

---
+++ src/components/policyitem/PolicyItem.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  policy: null,
+  isSub: {
+    type: Boolean,
+    default: false,
+  },
+});
+const baseUrl = import.meta.env.BASE_URL;
+</script>
+<template>
+  <div class="policyItem" :class="{ subItem: isSub }">
+    <p class="policyItem_text">{{ policy.text }}</p>
+    <img :src="`${baseUrl}${policy.image}`" alt="" />
+  </div>
+</template>
+<style scoped>
+img {
+  @apply rounded-2xl;
+}
+.policyItem {
+  @apply w-[30%] table:w-[48%] mobile:!w-max;
+  &_text {
+    @apply text-24s font-bold whitespace-pre-wrap p-4 pt-0 min-h-[5.5rem];
+  }
+}
+</style>
---
+++ src/components/policysection/PolicySection.vue
@@ -0,0 +1,40 @@
+<script setup>
+const policyData = reactive([
+  {
+    image: "imgs/policy1.png",
+    text: "為毛孩子謀福利！\n推動寵物醫療保障方案",
+  },
+  {
+    image: "imgs/policy2.png",
+    text: "打造休閒天堂\n推廣寵物休閒與娛樂場所",
+  },
+  {
+    image: "imgs/policy3.png",
+    text: "推廣寵物飼養教育，讓愛更加專業",
+  },
+]);
+</script>
+<template>
+  <section id="policy" class="section">
+    <div class="policySection">
+      <div class="mb-16 mobile:mb-10">
+        <h3 class="labelText">POLICY ISSUES</h3>
+        <h2>政策議題</h2>
+      </div>
+      <div class="policySection_content">
+        <PolicyItem v-for="policy in policyData" :key="policy" :policy="policy" />
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped>
+button {
+  @apply h-16 w-[9.5rem] py-4 px-6 gap-2  rounded-[500px] bg-[#F1F5F9] flex items-center justify-around;
+}
+.policySection {
+  @apply bg-white m-auto flex flex-col;
+  &_content {
+    @apply flex justify-between gap-8 table:flex-wrap mobile:gap-16;
+  }
+}
+</style>
