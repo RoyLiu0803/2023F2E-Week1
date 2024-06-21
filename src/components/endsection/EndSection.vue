409e5d1fc75461efb4bc0aa67b0636ecbfe58e93
RoyLiu <chance6687204@gmail.com>
2024-06-21 12:00:08 +0800

    feat: EndSection

---
+++ src/components/endsection/EndSection.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+<template>
+  <section id="donate" class="section">
+    <div class="donateSection table:whitespace-pre-line">
+      <h2>{{ "台灣的明天 \n喵先鋪路" }}</h2>
+      <div class="miaoLiHan">
+        <p class="circle3">3</p>
+        <p class="miaoLiHan_text">喵立翰 Miao Li-Han</p>
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped></style>
