ccfb840ea429e69dd9cad2160bfd050e07bb1e02
RoyLiu <chance6687204@gmail.com>
2024-06-21 11:59:33 +0800

    feat: EventSection

---
+++ src/components/eventitem/EventItem.vue
@@ -0,0 +1,53 @@
+<script setup>
+defineProps({
+  event: null,
+  isSub: {
+    type: Boolean,
+    default: false,
+  },
+});
+const baseUrl = import.meta.env.BASE_URL;
+</script>
+<template>
+  <div class="eventItem" :class="{ subItem: isSub }">
+    <div class="imgbox">
+      <img class="w-[calc(100vw-4.5rem)] table:max-w-none" :src="`${baseUrl}${event.image}`" alt="" />
+    </div>
+    <div class="eventItem_content">
+      <span class="eventItem_date">{{ event.date }}</span>
+      <span class="eventItem_title">{{ event.title }}</span>
+      <p class="eventItem_text">{{ event.text }}</p>
+    </div>
+  </div>
+</template>
+<style scoped>
+.subItem {
+  @apply inline-flex gap-2;
+  div {
+    @apply !pt-0 !text-16s;
+  }
+  .imgbox {
+    @apply w-[12.5rem] h-max rounded-2xl overflow-hidden mobile:my-auto;
+    img {
+      @apply w-[12.5rem];
+    }
+  }
+  .eventItem_content {
+    @apply max-w-[70%] mobile:max-w-[70%];
+  }
+}
+.eventItem {
+  &_content {
+    @apply pt-4 flex gap-2 text-textPrimary flex-col mobile:w-[calc(100vw-4.5rem)] mobile:gap-1;
+  }
+  &_date {
+    @apply text-14s text-[#64748B];
+  }
+  &_title {
+    @apply text-20s font-bold mobile:text-16s;
+  }
+  &_text {
+    @apply text-16s max-w-fit mobile:text-14s;
+  }
+}
+</style>
---
+++ src/components/eventsection/EventSection.vue
@@ -0,0 +1,59 @@
+<script setup>
+const eventData = reactive([
+  {
+    image: "imgs/news1.png",
+    date: "2023/12/26",
+    title: "參與台北寵物論壇，爭取貓咪友善環境",
+    text: "炎炎夏日的周六，我走進了台北寵物論壇，帶著一副貓耳髮箍，決定要全力宣傳「貓咪至上」的理念！我相信，我們的都市中，每一隻貓咪都應該有自己的 VIP 休憩空間。",
+  },
+  {
+    image: "imgs/news2.png",
+    date: "2023/12/24",
+    title: "掃街模式開啟！帶著你的貓耳，來和我一起走！",
+    text: "街上氣氛真的很棒，從小孩到大人，甚至有些狗狗朋友都帶著貓耳來找我握手，真的太可愛了！",
+  },
+  {
+    image: "imgs/news3.png",
+    date: "2023/12/20",
+    title: "收容所模特兒大比拼！",
+    text: "今天的收容所不再是一片寂靜。為了讓更多人認識到這裡的毛孩子，我們舉辦了一場前所未有的「模特兒走秀」！",
+  },
+  {
+    image: "imgs/news4.png",
+    date: "2023/12/26",
+    title: "參與台北寵物論壇，爭取貓咪友善環境",
+    text: "炎炎夏日的周六，我走進了台北寵物論壇，帶著一副貓耳髮箍，決定要全力宣傳「貓咪至上」的理念！",
+  },
+]);
+</script>
+<template>
+  <section id="event" class="section">
+    <div class="eventSection">
+      <div class="mb-16">
+        <h3 class="labelText">LATEST EVENTS</h3>
+        <h2>最新活動</h2>
+      </div>
+      <div class="eventSection_content">
+        <EventItem :event="eventData[0]" class="max-w-[50%]" />
+        <div class="flex flex-col gap-6">
+          <EventItem is-sub :event="eventData[1]" />
+          <EventItem is-sub :event="eventData[2]" />
+          <EventItem is-sub :event="eventData[3]" />
+          <button type="">查看更多 <img src="/imgs/arrow-right.svg" alt="" /></button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped>
+button {
+  @apply h-16 w-fit py-4 px-6 gap-2  rounded-[500px] bg-[#F1F5F9] flex items-center justify-around;
+}
+
+.eventSection {
+  @apply bg-white m-auto flex flex-col;
+  &_content {
+    @apply flex gap-8 table:flex-col;
+  }
+}
+</style>
